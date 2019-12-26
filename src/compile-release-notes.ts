6fca028322fcc244ae11737ce585a3d9f9c53e90
James <james@aehr.org>
2019-12-26 12:02:46 -0700

    Adds RN compilation, sorting, version grabbing, completed MVP!!! :cake:

--- src/compile-release-notes.ts
+++ src/compile-release-notes.ts
@@ -1,7 +1,44 @@
 import { PullsListResponseItem, IssuesGetResponse } from '@octokit/rest';
 
-export default function compileReleaseNotes (rels: { pr: PullsListResponseItem, issue: IssuesGetResponse[] }[]): String {
-  return rels.reduce((notes: String, rel) => {
-    return `${notes}\n${rel.pr.body}`;
+const RN_MATCHER = /\[RN\](.*)/i;
+
+// Hey! Ordering matters here for sorting of release notes
+const issueTypeRnMappings = {
+  feature: 'Feature',
+  enhancement: 'Enhancement',
+  bug: 'Squashed Bug'
+};
+
+const issueTypes = Object.keys(issueTypeRnMappings);
+
+type ReleaseLineItem = {
+  type: string,
+  note: string
+};
+
+export default function compileReleaseNotes (rels: { pr: PullsListResponseItem, issues?: IssuesGetResponse[] }[]): string {
+  const releaseLineItems: ReleaseLineItem[] = rels.map((rel): ReleaseLineItem => {
+    // Gather issue labels, determine first match for issue type
+    const issueType = rel.issues && rel.issues[0] && rel.issues[0].labels.length && (rel.issues[0].labels.find(issue =>
+      issueTypes.some(type => type === issue.name.toLowerCase())
+    ) || {}).name;
+
+    const issueTypeTitle = issueType ? issueTypeRnMappings[issueType] : 'Unknown';
+
+    const issueStr = rel.issues?.length ? rel.issues.map(iss => `#${iss.number}`).join(' ') : '',
+          releaseNotesMatches = RN_MATCHER.exec(rel.pr.body || '');
+
+    const rn = releaseNotesMatches && releaseNotesMatches[1] ? releaseNotesMatches[1] : rel.pr.body;
+
+    return {
+      type: issueType || 'unknown',
+      note: `**[${issueTypeTitle}]** ${rn.trim()} ${issueStr}`
+    };
+  });
+
+  return releaseLineItems.sort((a, b) => {
+    return issueTypes.indexOf(a.type) - issueTypes.indexOf(b.type);
+  }).reduce((notes: string, item) => {
+    return `${notes}\n${item.note}`.trim();
   }, '');
 };
--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,93 @@
 import { Application, Context } from 'probot';
+import inflector from 'i';
 import commands from 'probot-commands';
 import { PullsListResponseItem, IssuesGetResponse } from '@octokit/rest';
 import compileReleaseNotes from './compile-release-notes';
 import resolveIssueNumber from './resolve-issue-number';
+
+const { titleize } = inflector();
 
 type ReleaseIssue = {
   pr: PullsListResponseItem,
   issues?: IssuesGetResponse[]
 };
 
-export = (destroyAllHumans: Application) => {
-  commands(destroyAllHumans, 'releasenotes', async (context: Context, command: any) => {
-    const milestoneName = command.arguments;
+async function requestHandler (context: Context, command: any) {
+  const milestoneName = command.arguments;
 
-    if (!milestoneName) {
-      return;
-    }
+  if (!milestoneName) {
+    return;
+  }
 
-    const milestones = await context.github.issues.listMilestonesForRepo(context.issue({
-      state: 'open'
-    }));
-    
+  const milestones = await context.github.issues.listMilestonesForRepo(context.issue({
+    state: 'open'
+  }));
 
-    const milestone = milestones.data.find((ms) => ms.title.toLowerCase().replace(/\w/g, '') === milestoneName.toLowerCase().replace(/\w/g, ''));
-    console.log('milestones?');
-    console.log(milestone);
 
-    const prQuery = context.github.pulls.list.endpoint.merge(context.issue({
-      state: 'closed',
-    }));
+  const milestone = milestones.data.find((ms) => ms.title.toLowerCase().replace(/\w/g, '') === milestoneName.toLowerCase().replace(/\w/g, ''));
 
-    context.github.issues.get()
+  const prQuery = context.github.pulls.list.endpoint.merge(context.issue({
+    state: 'closed',
+  }));
 
-    const releaseIssues: ReleaseIssue[] = [];
+  const releaseIssues: ReleaseIssue[] = [];
 
-    for await (const response of context.github.paginate.iterator(prQuery)) {
-      for (let i = 0; i < response.data.length; i++) {
-        const pr = response.data[i];
+  for await (const response of context.github.paginate.iterator(prQuery)) {
+    for (let i = 0; i < response.data.length; i++) {
+      const pr = response.data[i];
 
-        if (pr.body.indexOf('[RN]') < 0) {
-          continue;
-        }
-
-        const closesIssues = resolveIssueNumber(pr.body);
-
-        let releaseItem: ReleaseIssue = {
-          pr,
-          issues: []
-        };
-
-        if (closesIssues.length) {
-          releaseItem.issues = await Promise.all(closesIssues.map(async (issNum: string) =>
-            (await context.github.issues.get(context.issue({
-              issue_number: issNum
-            }))).data
-          ));
-        }
-
-        releaseIssues.push(releaseItem);
+      if (pr.body.toLowerCase().indexOf('[rn]') < 0) {
+        continue;
       }
+
+      const closesIssues = resolveIssueNumber(pr.body);
+
+      let releaseItem: ReleaseIssue = {
+        pr,
+        issues: []
+      };
+
+      if (closesIssues.length) {
+        releaseItem.issues = await Promise.all(closesIssues.map(async (issNum: string) =>
+          (await context.github.issues.get(context.issue({
+            issue_number: issNum
+          }))).data
+        ));
+      }
+
+      releaseIssues.push(releaseItem);
     }
+  }
 
-    console.log('releaseissues', releaseIssues);
+  let repoVersion;
+
+  try {
+    const packageJsonReq = (await context.github.repos.getContents(context.repo({
+      path: 'package.json',
+      ref: context.payload.pull_request.head.ref
+    })));
 
 
-    // let commitList = await context.github.pulls.listCommits(context.issue());
-    // console.dir(commitList.data, { depth: 12 });
+    const data: any = packageJsonReq.data;
 
-    console.log('milestoneName', milestoneName);
-    // console.log(context.payload.pull_request);
-  });
+    if (data.content) {
+      repoVersion = JSON.parse(Buffer.from(data.content, data.encoding).toString()).version;
+    }
+    console.log(repoVersion);
+  } catch (e) {
+    context.log.error(`Unable to get package version: ${e}`);
+  }
+
+  const releaseNotesHeader = `## ${titleize(context.repo().repo)} ${repoVersion ? '@ ' + repoVersion : ''}`.trim(),
+        releaseNotes       = `${releaseNotesHeader}\n${compileReleaseNotes(releaseIssues)}`,
+        prBody             = context.payload.pull_request?.body,
+        reworkedPrBody     = prBody.replace(/\/releasenotes.*/, releaseNotes);
+
+  await context.github.pulls.update(context.issue({
+    body: reworkedPrBody
+  }));
+}
+
+export = (destroyAllHumans: Application) => {
+  commands(destroyAllHumans, 'releasenotes', requestHandler);
 };
